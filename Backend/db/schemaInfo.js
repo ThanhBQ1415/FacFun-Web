c8438913428385bb77d5dab7f99d1cb688f6ab87
ThanhBQ1415 <timvechanque7@gmail.com>
2024-10-09 21:17:32 +0700

    update

--- Backend/db/schemaInfo.js
+++ Backend/db/schemaInfo.js
@@ -1,16 +1,11 @@
 const mongoose = require("mongoose");
 
-/**
- * Create a Mongoose Schema.
- */
+
 const schemaInfo = new mongoose.Schema({
   version: String,
   load_date_time: { type: Date, default: Date.now },
 });
 
-/**
- * Create a Mongoose Model.
- */
 const SchemaInfo = mongoose.model("SchemaInfo", schemaInfo);
 
 
