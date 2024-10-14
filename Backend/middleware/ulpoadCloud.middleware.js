b8bb40684c13298169b41ab5c6fd346959a60250
ThanhBQ1415 <timvechanque7@gmail.com>
2024-10-14 08:36:33 +0700

    update

--- Backend/middleware/ulpoadCloud.middleware.js
+++ Backend/middleware/ulpoadCloud.middleware.js
@@ -3,13 +3,13 @@
 const cloudinary = require('cloudinary').v2
 const streamifier = require('streamifier')
 
-// cloudinary config
+
 cloudinary.config({ 
   cloud_name: process.env.cloud_name, 
   api_key: process.env.api_key, 
   api_secret: process.env.api_secret
 });
-// end cloudinary config
+
 
 module.exports.upload = (req, res, next) => {
     if(req.file){
