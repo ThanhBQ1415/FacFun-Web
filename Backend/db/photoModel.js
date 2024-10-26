1049103df29c7a987e7de52b450a88529bea140f
ThanhBQ1415 <timvechanque7@gmail.com>
2024-10-26 23:47:05 +0700

    u

--- Backend/db/photoModel.js
+++ Backend/db/photoModel.js
@@ -7,9 +7,7 @@ const commentSchema = new mongoose.Schema({
   user_id: mongoose.Schema.Types.ObjectId,
 });
 
-/**
- * Define the Mongoose Schema for a Photo.
- */
+
 const photoSchema = new mongoose.Schema({
   file_name: { type: String },
   date_time: { type: Date, default: Date.now },
